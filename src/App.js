35d340237bd7437b41cb733b73f639cdf07035f8
Marcin Mielniczuk <marcinmielni@gmail.com>
2022-06-01 14:44:09 +0200

    minoir changes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  useEffect(() => {
+    document.title = "MM - About"
+  }, [])
+
   return (
     <div className="App">
-      <header>
-        Marcin Mielniczuk
-      </header>
       <h1> Hello world!!!1111oneoneoe</h1>
     </div>
   );
